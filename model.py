dd9d49a2f20e7ad451de39378687da11ae119710
FantasyGeek <apoorvagarwal00@gmail.com>
2017-06-24 03:41:07 -0700

    model_slight_change

--- model.py
+++ model.py
@@ -29,195 +29,36 @@ from scipy.optimize import minimize
 from scipy.misc import imread, imsave, imresize
 import PIL.Image
 
+def style_transfer():
+    print('start')
+    neural_styler = NeuralStyler(content_image_path='input/maggie-grace-portrait-wallpapers_14105_1024x768.jpg',
+                                     style_image_path='output/darksideofthemoon.jpg',
 
-# def style_transfer(sourceImagePath, outputPath,filterPath):
-#     content_image_path = sourceImagePath
-#     style_image_path = filterPath
-#     height = 125
-#     width = 125
-#     # In[4]:
+                                     # If you have a local copy of Keras VGG16/19 weights
+                                     # weights_filepath='vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
-#     content_weights = [0.025]
-#     style_weights = [2.5]
-#     # content_weights = [0.025, 0.05, 0.75, 0.1]
-#     # style_weights = [2.0, 2.5, 3.0]
-#     total_variation_weights = [1.0]
+                                     Content_loss_function_weight=0.4,
+                                     Style_loss_function_weight=0.6,
+                                     verbose=True,
+                                     content_layer='block4_conv1',
+                                     style_layers=('block1_conv1',
+                                                   'block2_conv1',
+                                                   'block3_conv1',
+                                                   'block4_conv1',
+                                                   'block5_conv1'))
 
-#     # In[5]:
+        # Create styled image
+        #neural_styler.fit(canvas='picture', optimization_method='L-BFGS-B')
+        # or
+        # neural_styler.fit(canvas='picture', optimization_method='CG')
 
-
-#     style_image = Image.open(style_image_path)
-#     style_image = style_image.resize((height, width))
-
-#     # In[6]:
-
-
-#     content_image = Image.open(content_image_path)
-#     content_image = content_image.resize((height, width))
-
-#     # In[7]:
-
-
-#     content_array = np.asarray(content_image, dtype='float32')
-#     content_array = np.expand_dims(content_array, axis=0)
-#     print(content_array.shape)
-
-#     style_array = np.asarray(style_image, dtype='float32')
-#     style_array = np.expand_dims(style_array, axis=0)
-#     print(style_array.shape)
-
-#     # In[8]:
-
-
-#     content_array[:, :, :, 0] -= 103.939
-#     content_array[:, :, :, 1] -= 116.779
-#     content_array[:, :, :, 2] -= 123.68
-#     content_array = content_array[:, :, :, ::-1]
-
-#     style_array[:, :, :, 0] -= 103.939
-#     style_array[:, :, :, 1] -= 116.779
-#     style_array[:, :, :, 2] -= 123.68
-#     style_array = style_array[:, :, :, ::-1]
-
-#     # In[13]:
-
-
-#     def content_loss(content, combination):
-#         return backend.sum(backend.square(combination - content))
-
-#     # In[14]:
-
-
-#     def gram_matrix(x):
-#         features = backend.batch_flatten(backend.permute_dimensions(x, (2, 0, 1)))
-#         gram = backend.dot(features, backend.transpose(features))
-#         return gram
-
-#     # In[15]:
-
-
-#     def style_loss(style, combination):
-#         S = gram_matrix(style)
-#         C = gram_matrix(combination)
-#         channels = 3
-#         size = height * width
-#         return backend.sum(backend.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
-
-#     # In[16]:
-
-
-#     def total_variation_loss(x):
-#         a = backend.square(x[:, :height - 1, :width - 1, :] - x[:, 1:, :width - 1, :])
-#         b = backend.square(x[:, :height - 1, :width - 1, :] - x[:, :height - 1, 1:, :])
-#         return backend.sum(backend.pow(a + b, 1.25))
-
-#     # In[18]:
-
-
-
-
-#     def eval_loss_and_grads(x):
-#         x = x.reshape((1, height, width, 3))
-#         outs = f_outputs([x])
-#         loss_value = outs[0]
-#         grad_values = outs[1].flatten().astype('float64')
-#         return loss_value, grad_values
-
-#     class Evaluator(object):
-
-#         def __init__(self):
-#             self.loss_value = None
-#             self.grads_values = None
-
-#         def loss(self, x):
-#             assert self.loss_value is None
-#             loss_value, grad_values = eval_loss_and_grads(x)
-#             self.loss_value = loss_value
-#             self.grad_values = grad_values
-#             return self.loss_value
-
-#         def grads(self, x):
-#             assert self.loss_value is not None
-#             grad_values = np.copy(self.grad_values)
-#             self.loss_value = None
-#             self.grad_values = None
-#             return grad_values
-
-#     # In[20]:
-
-
-#     content_image = backend.variable(content_array)
-#     style_image = backend.variable(style_array)
-#     combination_image = backend.placeholder((1, height, width, 3))
-
-#     input_tensor = backend.concatenate([content_image,
-#                                         style_image,
-#                                         combination_image], axis=0)
-
-#     model = VGG16(input_tensor=input_tensor, weights='imagenet',
-#                   include_top=False)
-#     layers = dict([(layer.name, layer.output) for layer in model.layers])
-
-#     # In[21]:
-
-
-#     images = list()
-#     for content_weight in content_weights:
-#         for style_weight in style_weights:
-#             for total_variation_weight in total_variation_weights:
-
-#                 loss = backend.variable(0.)
-
-#                 layer_features = layers['block2_conv2']
-#                 content_image_features = layer_features[0, :, :, :]
-#                 combination_features = layer_features[2, :, :, :]
-
-#                 loss += content_weight * content_loss(content_image_features, combination_features)
-
-#                 feature_layers = ['block1_conv2', 'block2_conv2',
-#                                   'block3_conv3', 'block4_conv3',
-#                                   'block5_conv3']
-#                 for layer_name in feature_layers:
-#                     layer_features = layers[layer_name]
-#                     style_features = layer_features[1, :, :, :]
-#                     combination_features = layer_features[2, :, :, :]
-#                     sl = style_loss(style_features, combination_features)
-#                     loss += (style_weight / len(feature_layers)) * sl
-
-#                 loss += total_variation_weight * total_variation_loss(combination_image)
-#                 grads = backend.gradients(loss, combination_image)
-
-#                 outputs = [loss]
-#                 outputs += grads
-#                 f_outputs = backend.function([combination_image], outputs)
-
-#                 evaluator = Evaluator()
-
-#                 x = np.random.uniform(0, 255, (1, height, width, 3)) - 128.
-
-#                 iterations = 10
-
-#                 for i in range(iterations):
-#                     print('Start of iteration', i)
-#                     start_time = time.time()
-#                     x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(), fprime=evaluator.grads, maxfun=20)
-#                     print('Current loss value:', min_val)
-#                     end_time = time.time()
-#                     print('Iteration %d completed in %ds' % (i, end_time - start_time))
-
-#                 x = x.reshape((height, width, 3))
-#                 x = x[:, :, ::-1]
-#                 x[:, :, 0] += 103.939
-#                 x[:, :, 1] += 116.779
-#                 x[:, :, 2] += 123.68
-#                 x = np.clip(x, 0, 255).astype('uint8')
-#                 images.append(Image.fromarray(x))
-#                 print("With Content_weight = ", content_weight)
-#                 print("With Style Weight = ", style_weight)
-
-#     # In[22]:
-
-#     images[0].save(outputPath)
+        # Try also
+        #
+    neural_styler.fit(canvas='random_from_style', optimization_method='TNC')
+        # and
+        # neural_styler.fit(canvas='style')
+        #
+        # with different optimization algorithms (CG, etc.)
 
 
 class NeuralStyler(object):
@@ -443,34 +284,3 @@ class NeuralStyler(object):
         return np.clip(image[:, :, :, ::-1], 0, 255).astype('uint8')[0]
 
 
-
-def style_transfer():
-    print('start')
-    neural_styler = NeuralStyler(content_image_path='input/maggie-grace-portrait-wallpapers_14105_1024x768.jpg',
-                                     style_image_path='output/darksideofthemoon.jpg',
-
-                                     # If you have a local copy of Keras VGG16/19 weights
-                                     # weights_filepath='vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-
-                                     Content_loss_function_weight=0.4,
-                                     Style_loss_function_weight=0.6,
-                                     verbose=True,
-                                     content_layer='block4_conv1',
-                                     style_layers=('block1_conv1',
-                                                   'block2_conv1',
-                                                   'block3_conv1',
-                                                   'block4_conv1',
-                                                   'block5_conv1'))
-
-        # Create styled image
-        #neural_styler.fit(canvas='picture', optimization_method='L-BFGS-B')
-        # or
-        # neural_styler.fit(canvas='picture', optimization_method='CG')
-
-        # Try also
-        #
-    neural_styler.fit(canvas='random_from_style', optimization_method='TNC')
-        # and
-        # neural_styler.fit(canvas='style')
-        #
-        # with different optimization algorithms (CG, etc.)
